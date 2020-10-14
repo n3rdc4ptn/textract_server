c6cd43ae973b98df9c06525b3b66324ec9c725e7
Moritz Reich <moritzreich@Moritzs-MBP.fritz.box>
2020-10-14 13:58:09 +0200

    added PDF_MAX_PAGES env

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 var express = require('express');
 var app = express();
 const fileUpload = require('express-fileupload');
+var textract = require('textract');
+var PDFINFO = require('pdfinfo');
 
 var fs = require('fs');
 
+function extractText(filename, res) {
+    textract.fromFileWithPath(filename, {
+        preserveLineBreaks: true,
+        pdftotextOptions: {
+            lastPage: parseInt(process.env.PDF_MAX_PAGES)
+        }
+    }, function(err, text) {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+
+        fs.unlinkSync(filename);
+        res.send(text);
+    });
+}
+
 app.use(fileUpload());
 
-
-var textract = require('textract');
-
-app.get('/upload', function(req, res) {
-    console.log(req.query);
-    console.log(req.files);
-    res.send(
-        req.query.callback + "('"+Test+"');" +
-        "console.log('Test');"
-    );
-})
-
 app.post('/upload', function(req, res) {
-    console.log(req.files); // the uploaded file object
+    // console.log(req.files); // the uploaded file object
 
     var filename = "tmp_files/" + req.files.analysefile.md5 + "_" + req.files.analysefile.name;
     req.files.analysefile.mv(filename);
@@ -27,21 +34,24 @@ app.post('/upload', function(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    textract.fromFileWithPath(filename, {
-        preserveLineBreaks: true   
-    }, function(err, text) {
-        if (err) {
-            console.log(err);
-            res.status(500).send(err);
-        }
 
-        res.send(text);
-        console.log("1");
-        // fs.unlink(filename, (err) => {
-        //     if (err) throw err;
-        // });
-    });
-    console.log("2");
+    if (process.env.PDF_MAX_PAGES && req.files.analysefile.mimetype=="application/pdf") {
+        var pdf = PDFINFO(filename);
+        pdf.info((err, meta) => {
+            if (err) throw err;
+            if (meta.pages > parseInt(process.env.PDF_MAX_PAGES)) {
+                res.send("too-many-pages");
+
+                fs.unlinkSync(filename);
+                return;
+            }
+
+            extractText(filename, res);
+        })
+    } else {
+        extractText(filename, res);
+    }
+
 
     // res.send("POST");
 });
